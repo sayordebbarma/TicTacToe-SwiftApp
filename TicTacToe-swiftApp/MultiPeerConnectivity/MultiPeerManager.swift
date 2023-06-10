51d6c82c3d5fedd8bca2eea8d5ef9401c35258aa
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-06-10 22:53:10 +0530

    setting up multiPeerManager

--- TicTacToe-swiftApp/MultiPeerConnectivity/MultiPeerManager.swift
+++ TicTacToe-swiftApp/MultiPeerConnectivity/MultiPeerManager.swift
@@ -17,12 +17,30 @@ class MPConnectionManager: NSObject, ObservableObject {
     let myPeerId: MCPeerID
     let nearbyServiceAdvertiser: MCNearbyServiceAdvertiser
     let nearbyServiceBrowser: MCNearbyServiceBrowser
+    @Published var availablePeers = [MCPeerID]()
+    @Published var receiveInvite: Bool = false
+    @Published var receiveInviteForm: MCPeerID?
+    @Published var invitationHandler: ((Bool, MCSession?) -> Void)?
+    @Published var paired: Bool = false
+    var isAvailableToPlay: Bool = false {
+        didSet {
+            if isAvailableToPlay {
+                startAdvertising()
+            } else {
+                stopAdvertising()
+            }
+        }
+    }
     
     init(yourName: String) {
         myPeerId = MCPeerID(displayName: yourName)
         session = MCSession(peer: myPeerId)
         nearbyServiceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: serviceType)
         nearbyServiceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: serviceType)
+        super.init()
+        session.delegate = self
+        nearbyServiceBrowser.delegate = self
+        nearbyServiceAdvertiser.delegate = self
     }
     
     deinit {
@@ -41,5 +59,71 @@ class MPConnectionManager: NSObject, ObservableObject {
     }
     func stopBrowsing() {
         nearbyServiceBrowser.stopBrowsingForPeers()
+        availablePeers.removeAll()
+    }
+}
+
+extension MPConnectionManager: MCNearbyServiceBrowserDelegate {
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        DispatchQueue.main.async {
+            if !self.availablePeers.contains(peerID) {
+                self.availablePeers.append(peerID)
+            }
+        }
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        guard let index = availablePeers.firstIndex(of: peerID) else { return }
+        DispatchQueue.main.async {
+            self.availablePeers.remove(at: index)
+        }
+    }
+}
+
+extension MPConnectionManager: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        DispatchQueue.main.async {
+            self.receiveInvite = true
+            self.receiveInviteForm = peerID
+            self.invitationHandler = invitationHandler
+        }
+    }
+}
+
+extension MPConnectionManager: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .notConnected:
+            DispatchQueue.main.async {
+                self.paired = false
+                self.isAvailableToPlay = true
+            }
+        case .connected:
+            DispatchQueue.main.async {
+                self.paired = true
+                self.isAvailableToPlay = false
+            }
+        default:
+            DispatchQueue.main.async {
+                self.paired = false
+                self.isAvailableToPlay = true
+            }
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        <#code#>
     }
 }
